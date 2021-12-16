cf51d932dcbe9eaab1839418a593a05160087a5d
Aleksei Krasnov <alexey-krasnov@rambler.ru>
2021-12-16 17:31:53 +0300

    Electric started project

--- electric_processing.py
+++ electric_processing.py
@@ -1,54 +1,58 @@
-import pandas as pd
+import os
 import glob
 import numpy as np
-import os
-from timeit import timeit
+import pandas as pd
 
 
 def get_user_input():
-    global h, d, s, c_0
-    """Get the thickness and diameter of the sample from user. Calculate surface area and vacuum capacity"""
-    # Get the thickness and diameter of the sample in mm, transform them in m (SI)
+    global H, D, S, C_0
+    """Get the thickness and diameter of the sample from user in mm, transform them in m (SI). 
+    Calculate surface area and vacuum capacity"""
     while True:
         try:
-            h = float(input("Enter the thickness of the sample in mm: ")) / 1000
-            d = float(input("Enter the diameter of the sample in mm: ")) / 1000
+            H = float(input("Enter the thickness of the sample in mm: ")) / 1000
+            D = float(input("Enter the diameter of the sample in mm: ")) / 1000
         except ValueError:
             print("The values should be only digits, e.g. 1.2 and 13.5.")
         else:
             # Calculate the surface area of the sample in m^2
-            s = (np.pi * d * d) / 4
+            S = (np.pi * D * D) / 4
             # Calculate vacuum capacity
-            c_0 = ((8.854 * (10 ** -12)) * s) / h
+            C_0 = ((8.854 * (10 ** -12)) * S) / H
             break
 
 
-def data_processing():
-    """Read data from the raw csv files in the current directory, processing, and writing data in the DataFrame.
+def data_reading():
+    """Read data from the raw csv files in the current directory.
     Please, check the input files, they should be in form of columns separated with ';'."""
     global df
     colnames = ['f', '|Z|', '-φ']  # Assign column names
     df = pd.read_csv(i, names=colnames, sep=";")
+
+
+def data_processing():
+    """Processing, and writing data in the DataFrame."""
+    global H, D
     phi_radian = (df['-φ'] * np.pi * -1) / 180   # Transform phase angle into radian
     # Calculation of the corresponding electrophysical values.
     df['Z\''] = df['|Z|'] * np.cos(phi_radian)  # Real part of the impedance modulus|Z|
     df["Z\""] = df['|Z|'] * np.sin(phi_radian)  # Imaginary part of the impedance modulus|Z|
-    df['Z\', Om·cm'] = df['|Z|'] * np.cos(phi_radian) * 100 * s / h  # Specific real part of the impedance modulus
-    df["Z\", Om·cm"] = df['|Z|'] * np.sin(phi_radian) * 100 * s / h  # Specific imaginary part of the impedance modulus
+    df['Z\', Om·cm'] = df['|Z|'] * np.cos(phi_radian) * 100 * S / H # Specific real part of the impedance modulus
+    df["Z\", Om·cm"] = df['|Z|'] * np.sin(phi_radian) * 100 * S / H # Specific imaginary part of the impedance modulus
     df['logf'] = np.log10(df['f'])  # lg of frequency
     df['ω'] = 2 * np.pi * df['f']  # circular frequency
     df['Cu'] = df["Z\""] / (df['ω'] * ((df['Z\''])**2 + (df["Z\""])**2))  # real capacity
     df['φ'] = df['-φ'] * (-1)  # Positive phase angle
     df['σu'] = df['Z\''] / ((df['Z\''])**2 + (df["Z\""])**2)  # Conductivity
-    df['σspec, Sm/cm'] = (df['σu'] * h * 0.01) / s  # Specific conductivity in Sm/cm
+    df['σspec, Sm/cm'] = (df['σu'] * H* 0.01) / S  # Specific conductivity in Sm/cm
     df['logσspec'] = np.log10(df['σspec, Sm/cm'])  # lg of specific conductivity
-    df['ε\''] = df['Cu'] / c_0  # Real part of the dielectric constant
-    df['ε\"'] = df['σu'] / (df['ω'] * c_0)  # Imaginary part of the dielectric constant
+    df['ε\''] = df['Cu'] / C_0  # Real part of the dielectric constant
+    df['ε\"'] = df['σu'] / (df['ω'] * C_0)  # Imaginary part of the dielectric constant
     df['β\''] = 1 / df['ε\'']
     df['β\"'] = 1 / df['ε\"']
     df['tanδ'] = df['ε\"'] / df['ε\'']  # Dielectric loss tangent
-    df['M\''] = df['ω'] * c_0 * df["Z\""]  # Real part of the electric modulus
-    df['M\"'] = df['ω'] * c_0 * df['Z\'']  # Imaginary part of the electric modulus
+    df['M\''] = df['ω'] * C_0 * df["Z\""]  # Real part of the electric modulus
+    df['M\"'] = df['ω'] * C_0 * df['Z\'']  # Imaginary part of the electric modulus
 
 
 def export_data_excel():
@@ -61,24 +65,27 @@ def export_data_excel():
 def export_data_zview():
     """Make a directory and export data as txt files for Zview processing. The format of txt file is f, Z', -Z". """
     df["-Z\", Om·cm"] = df["Z\", Om·cm"] * (-1)
-    df.to_csv(f'{outdir}/{i}', columns=['f', 'Z\', Om·cm', "-Z\", Om·cm"], sep=' ', index=False, header=None)
+    df.to_csv(f'{out_dir}/{i}', columns=['f', 'Z\', Om·cm', "-Z\", Om·cm"], sep=' ', index=False, header=None)
 
 
 current_dir = os.path.basename(os.getcwd())  # Get name of current directory
-outdir = 'Zview_files'  # Directory for Zview out files
+out_dir = 'Zview_files'  # Directory for Zview out files
 
 # Check if you have already run the program and got the files.
-if f'{current_dir}.xlsx' and outdir in glob.glob('*'):
+# if f'{current_dir}.xlsx' and out_dir in glob.glob('*'):
+if os.path.exists(out_dir) and glob.glob('*.xlsx'):
     print("You have already generated necessary files.")
 else:
     get_user_input()
-    os.mkdir(outdir)
-    with pd.ExcelWriter(f'{current_dir}_h={h}_d={d}.xlsx') as writer:
-        for i in sorted(glob.glob('*.txt')):
-            data_processing()
-            export_data_excel()
-            export_data_zview()
-    print("Processing of your absorption data is finished successfully!")
-# print("data_processing takes", timeit(data_processing, number=1000), 'seconds')
-# print("export_data_excel takes", timeit(export_data_excel, number=1000), 'seconds')
-# print("export_data_zview takes", timeit(export_data_zview, number=1000), 'seconds')
+    try:
+        os.mkdir(out_dir)
+    except FileExistsError:
+        pass
+    finally:
+        with pd.ExcelWriter(f'{current_dir}_h={H}_d={D}.xlsx') as writer:
+            for i in sorted(glob.glob('*.txt')):
+                data_reading()
+                data_processing()
+                export_data_excel()
+                export_data_zview()
+        print("Processing of your absorption data is finished successfully!")
--- electric_tkinter.py
+++ electric_tkinter.py
@@ -1,117 +1,142 @@
-import pandas as pd
-import glob
-from math import pi
-import numpy as np
-import os
+# import pandas as pd
+# import glob
+# from math import pi
+# import numpy as np
+# import os
 from tkinter import *
+from tkinter import filedialog as fd
+#
+# def get_user_input():
+#     global h, d, s, c_0
+#     """Get the thickness and diameter of the sample from user. Calculate surface area and vacuum capacity"""
+#     # Get the thickness and diameter of the sample in mm, transform them in m
+#     while True:
+#         try:
+#             h = float(input("Enter the thickness of the sample in mm: ")) / 1000
+#             d = float(input("Enter the diameter of the sample in mm: ")) / 1000
+#         except ValueError:
+#             print("The values should be only digits, e.g. 1.2 and 13.5.")
+#         else:
+#             # Calculate the surface area of the sample in m^2
+#             s = (pi * d * d) / 4
+#             # Calculate vacuum capacity
+#             c_0 = ((8.854 * (10 ** -12)) * s) / h
+#             break
+#
+#
+# def data_processing():
+#     """Read data from the raw csv files in the current directory, processing, and writing data in the DataFrame."""
+#     global df
+#     colnames = ['f', '|Z|', '-φ']  # Assign column names
+#     df = pd.read_csv(i, names=colnames, sep=";")
+#     phi_radian = (df['-φ'] * pi * -1) / 180   # Transform phase angle into radian
+#     # Calculation of the corresponding electrophysical values.
+#     df['Z\''] = df['|Z|'] * np.cos(phi_radian)  # Real part of impedance Z
+#     df["Z\""] = df['|Z|'] * np.sin(phi_radian)  # Imaginary part of impedance
+#     df['Z\', Om·cm'] = df['|Z|'] * np.cos(phi_radian) * 100 * s / h  # Specific real part of impedance Z
+#     df["Z\", Om·cm"] = df['|Z|'] * np.sin(phi_radian) * 100 * s / h  # Specific imaginary part of impedance Z
+#     df['logf'] = np.log10(df['f'])  # lg of frequency
+#     df['ω'] = 2 * pi * df['f']  # circular frequency
+#     df['Cu'] = df["Z\""] / (df['ω'] * ((df['Z\''])**2 + (df["Z\""])**2))  # real capacity
+#     df['φ'] = df['-φ'] * (-1)  # Positive phase angle
+#     df['σu'] = df['Z\''] / ((df['Z\''])**2 + (df["Z\""])**2)  # Conductivity
+#     df['σspec, Sm/cm'] = (df['σu'] * h * 0.01) / s  # Specific conductivity in Sm/cm
+#     df['ε\''] = df['Cu'] / c_0  # real part of dielectric constant
+#     df['ε\"'] = df['σu'] / (df['ω'] * c_0)  # imaginary part od dielectric constant
+#     df['β\''] = 1 / df['ε\'']
+#     df['β\"'] = 1 / df['ε\"']
+#     df['tanδ'] = df['ε\"'] / df['ε\'']  # dielectric loss tangent
+#     df['M\''] = df['ω'] * c_0 * df["Z\""]  # real part of electric modulus
+#     df['M\"'] = df['ω'] * c_0 * df['Z\'']  # imaginary part of electric modulus
+#
+#
+# def export_data_excel():
+#     """Create one excel file and store the electrophysical values at one temperature as the corresponding sheet."""
+#     df.to_excel(writer, sheet_name=f'{i.replace(".txt", "")}', index=False,
+#                 columns=['f', 'Z\', Om·cm', "Z\", Om·cm", 'logf', 'ω', 'Cu', 'φ', 'σu', 'σspec, Sm/cm',
+#                          'ε\'', 'ε\"', 'β\'', 'β\"', 'tanδ', 'M\'', 'M\"'])
+#
+#
+# def export_data_zview():
+#     """Make a directory ans export data as txt files for Zview processing. The format of txt file is f, Z', -Z". """
+#     df["-Z\", Om·cm"] = df["Z\", Om·cm"] * (-1)
+#     df.to_csv(f'{outdir}/{i}', columns=['f', 'Z\', Om·cm', "-Z\", Om·cm"], sep=' ', index=False, header=None)
+#
+#
+# current_dir = os.path.basename(os.getcwd())  # Get name of current directory
+# outdir = 'Zview_files'  # Directory for Zview out files
+# # Check if you have already run the program and got the files.
+# if f'{current_dir}.xlsx' and outdir in glob.glob('*'):
+#     print("You have already generated necessary files.")
+# else:
+#     get_user_input()
+#     os.mkdir(outdir)
+#     with pd.ExcelWriter(f'{current_dir}_h={h}_d={d}.xlsx') as writer:
+#         for i in sorted(glob.glob('*.txt')):
+#             data_processing()
+#             export_data_excel()
+#             export_data_zview()
+#     print("Processing of your absorption data is finished successfully!")
+#
 
-def get_user_input():
-    global h, d, s, c_0
-    """Get the thickness and diameter of the sample from user. Calculate surface area and vacuum capacity"""
-    # Get the thickness and diameter of the sample in mm, transform them in m
-    while True:
-        try:
-            h = float(input("Enter the thickness of the sample in mm: ")) / 1000
-            d = float(input("Enter the diameter of the sample in mm: ")) / 1000
-        except ValueError:
-            print("The values should be only digits, e.g. 1.2 and 13.5.")
-        else:
-            # Calculate the surface area of the sample in m^2
-            s = (pi * d * d) / 4
-            # Calculate vacuum capacity
-            c_0 = ((8.854 * (10 ** -12)) * s) / h
-            break
-
-
-def data_processing():
-    """Read data from the raw csv files in the current directory, processing, and writing data in the DataFrame."""
-    global df
-    colnames = ['f', '|Z|', '-φ']  # Assign column names
-    df = pd.read_csv(i, names=colnames, sep=";")
-    phi_radian = (df['-φ'] * pi * -1) / 180   # Transform phase angle into radian
-    # Calculation of the corresponding electrophysical values.
-    df['Z\''] = df['|Z|'] * np.cos(phi_radian)  # Real part of impedance Z
-    df["Z\""] = df['|Z|'] * np.sin(phi_radian)  # Imaginary part of impedance
-    df['Z\', Om·cm'] = df['|Z|'] * np.cos(phi_radian) * 100 * s / h  # Specific real part of impedance Z
-    df["Z\", Om·cm"] = df['|Z|'] * np.sin(phi_radian) * 100 * s / h  # Specific imaginary part of impedance Z
-    df['logf'] = np.log10(df['f'])  # lg of frequency
-    df['ω'] = 2 * pi * df['f']  # circular frequency
-    df['Cu'] = df["Z\""] / (df['ω'] * ((df['Z\''])**2 + (df["Z\""])**2))  # real capacity
-    df['φ'] = df['-φ'] * (-1)  # Positive phase angle
-    df['σu'] = df['Z\''] / ((df['Z\''])**2 + (df["Z\""])**2)  # Conductivity
-    df['σspec, Sm/cm'] = (df['σu'] * h * 0.01) / s  # Specific conductivity in Sm/cm
-    df['ε\''] = df['Cu'] / c_0  # real part of dielectric constant
-    df['ε\"'] = df['σu'] / (df['ω'] * c_0)  # imaginary part od dielectric constant
-    df['β\''] = 1 / df['ε\'']
-    df['β\"'] = 1 / df['ε\"']
-    df['tanδ'] = df['ε\"'] / df['ε\'']  # dielectric loss tangent
-    df['M\''] = df['ω'] * c_0 * df["Z\""]  # real part of electric modulus
-    df['M\"'] = df['ω'] * c_0 * df['Z\'']  # imaginary part of electric modulus
-
-
-def export_data_excel():
-    """Create one excel file and store the electrophysical values at one temperature as the corresponding sheet."""
-    df.to_excel(writer, sheet_name=f'{i.replace(".txt", "")}', index=False,
-                columns=['f', 'Z\', Om·cm', "Z\", Om·cm", 'logf', 'ω', 'Cu', 'φ', 'σu', 'σspec, Sm/cm',
-                         'ε\'', 'ε\"', 'β\'', 'β\"', 'tanδ', 'M\'', 'M\"'])
-
-
-def export_data_zview():
-    """Make a directory ans export data as txt files for Zview processing. The format of txt file is f, Z', -Z". """
-    df["-Z\", Om·cm"] = df["Z\", Om·cm"] * (-1)
-    df.to_csv(f'{outdir}/{i}', columns=['f', 'Z\', Om·cm', "-Z\", Om·cm"], sep=' ', index=False, header=None)
-
-
-current_dir = os.path.basename(os.getcwd())  # Get name of current directory
-outdir = 'Zview_files'  # Directory for Zview out files
-# Check if you have already run the program and got the files.
-if f'{current_dir}.xlsx' and outdir in glob.glob('*'):
-    print("You have already generated necessary files.")
-else:
-    get_user_input()
-    os.mkdir(outdir)
-    with pd.ExcelWriter(f'{current_dir}_h={h}_d={d}.xlsx') as writer:
-        for i in sorted(glob.glob('*.txt')):
-            data_processing()
-            export_data_excel()
-            export_data_zview()
-    print("Processing of your absorption data is finished successfully!")
-
-# GUI interface
+# GUI
 root = Tk()
-# Set geometry
-root.geometry('800x400')
-# Add Labels, Button and Frames
-Label(root, text='Electric processing', font="Helvetica 20 bold", fg="Green").pack(pady=10)
+root.title("Electric Processing")
+root.geometry('400x400+100+200')
+root.resizable(width=True, height=True)
+H = StringVar()  # get thickness of the sample
+D = StringVar()  # get diameter of the sample
 
-# Frame 1
-frame1 = Frame(root)
-Label(frame1, text='Type Word', font="Helvetica 15 bold").pack(side=LEFT)
-word = Entry(frame1, font='Helvetica 15 bold')
-word.pack()
-frame1.pack(pady=10)
+def get_sample_size():
+    get_thikness = h_entry.get()
+    get_diameter = d_entry.get()
+    H = float(get_thikness)
+    D = float(get_diameter)
 
-# Frame 2
-frame2 = Frame(root)
-Label(frame2, text='Meaning', font="Helvetica 10 bold").pack(side=LEFT)
-meaning = Label(frame2, text='', font='Helvetica 10')
-meaning.pack()
-frame2.pack(pady=10)
+l_1 = Label(root, text='Enter the thikness of the sample in mm: ')
+l_1.grid(row=1, column=1)
 
-# Frame 3
-frame3 = Frame(root)
-Label(frame3, text='Synonym', font="Helvetica 10 bold").pack(side=LEFT)
-synonym = Label(frame3, text='', font='Helvetica 10')
-synonym.pack()
-frame3.pack(pady=10)
+h_entry = Entry(root, width=10, textvariable=H)
+h_entry.grid(row=1, column=2, padx=5, pady=5)
 
-# Frame 4
-frame4 = Frame(root)
-Label(frame4, text='Antonym', font="Helvetica 10 bold").pack(side=LEFT)
-antonym = Label(frame4, text='', font='Helvetica 10')
-antonym.pack()
-frame4.pack(pady=10)
+l_2 = Label(root, text='Enter the diameter of the sample in mm: ')
+l_2.grid(row=2, column=1, padx=5, pady=5)
 
-#  Make Button key
-Button(root, text="Submit", font="Helvetica 15 bold", command=dict).pack()
+d_entry = Entry(root, width=10, textvariable=D)
+d_entry.grid(row=2, column=2, padx=5, pady=5)
+
+b_user_size = Button(root, text='OK', command=get_sample_size)
+b_user_size.grid(row=3, column=2, padx=5, pady=5)
+
+l_3 = Label(root, text='Choose files from the directory: ')
+l_3.grid(row=4, column=1, padx=5, pady=5)
+# l_test_out = Label(root)
+# l_test_out.grid(row=4, column=2, padx=5, pady=5)
+
+def insert_text():
+    file_name = fd.askopenfilename()
+    f = open(file_name)
+    s = f.read()
+    text.insert(1.0, s)
+    f.close()
+
+def extract_text():
+    file_name = fd.asksaveasfilename(
+        filetypes=(("TXT files", "*.txt"),
+                   ("HTML files", "*.html;*.htm"),
+                   ("All files", "*.*")))
+    f = open(file_name, 'w')
+    s = text.get(1.0, END)
+    f.write(s)
+    f.close()
+
+text = Text(width=20, height=2)
+text.grid(row=4, column=2,columnspan=2)
+b1 = Button(text="Открыть", command=insert_text)
+b1.grid(row=5, column=1, sticky=E)
+b2 = Button(text="Сохранить", command=extract_text)
+b2.grid(row=5, column=2, sticky=W)
+
+
+
 root.mainloop()
