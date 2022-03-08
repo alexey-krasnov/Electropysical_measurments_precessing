c466dcc5cb19043467992aed4a1ef5a12355b5a8
Aleksei Krasnov <alexey-krasnov@rambler.ru>
2022-03-08 18:00:46 +0300

    First major refractoring

--- electric_processing.py
+++ electric_processing.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 
 
-def get_user_input():
-    global H, D, S, C_0
+def get_user_input() -> tuple:
+    global H, D
     """Get the thickness and diameter of the sample from user in mm, transform them in m (SI). 
     Calculate surface area and vacuum capacity."""
     while True:
@@ -20,17 +20,19 @@ def get_user_input():
             # Calculate vacuum capacity
             C_0 = ((8.854 * (10**-12)) * S) / H
             break
+    return S, C_0
 
 
 def data_reading():
     """Read data from the raw csv files in the current directory.
     Please, check the input files, they should be in form of columns separated with ';'."""
-    global df
+    # global df
     colnames = ['f', '|Z|', '-φ']  # Assign column names
     df = pd.read_csv(i, names=colnames, sep=";")
+    return df
 
 
-def data_processing():
+def data_processing(df, S, C_0):
     """Processing, calculation of the corresponding electrophysical values. Collecting data in the DataFrame."""
     phi_radian = (df['-φ'] * np.pi * -1) / 180   # Transform phase angle into radian
     df['Z\''] = df['|Z|'] * np.cos(phi_radian)  # Real part of the impedance modulus|Z|
@@ -51,48 +53,57 @@ def data_processing():
     df['tanδ'] = df['ε\"'] / df['ε\'']  # Dielectric loss tangent
     df['M\''] = df['ω'] * C_0 * df["Z\""]  # Real part of the electric modulus
     df['M\"'] = df['ω'] * C_0 * df['Z\'']  # Imaginary part of the electric modulus
+    return df
 
 
-def export_data_excel():
+def export_data_excel(df):
     """Create one Excel file and store the electrophysical values at one temperature as the corresponding sheet."""
     df.to_excel(writer, sheet_name=f'{i.replace(".txt", "")}', index=False,
                 columns=['f', 'Z\', Om·cm', "Z\", Om·cm", 'logf', 'ω', 'Cu', 'φ', 'σu', 'σspec, Sm/cm', 'logσspec',
                          'ε\'', 'ε\"', 'β\'', 'β\"', 'tanδ', 'M\'', 'M\"'])
 
 
-def export_data_zview():
+def export_data_zview(df, dir_name):
     """Make a directory and export data as txt files for Zview processing. The format of txt file is f, Z', -Z". """
     df["-Z\", Om·cm"] = df["Z\", Om·cm"] * (-1)
-    df.to_csv(f'{out_dir}/{i}', columns=['f', 'Z\', Om·cm', "-Z\", Om·cm"], sep=' ', index=False, header=None)
+    df.to_csv(f'{dir_name}/{i}', columns=['f', 'Z\', Om·cm', "-Z\", Om·cm"], sep=' ', index=False, header=None)
 
 
-def export_data_as_txt():
+def export_data_as_txt(df, dir_name):
     """Make a directory and export data as txt files with the electrophysical values
     for further plotting processing. """
-    df.to_csv(f'{data_dir}/{i}', columns=['f', 'Z\', Om·cm', "Z\", Om·cm", 'logf', 'ω', 'Cu', 'φ', 'σu', 'σspec, Sm/cm',
+    df.to_csv(f'{dir_name}/{i}', columns=['f', 'Z\', Om·cm', "Z\", Om·cm", 'logf', 'ω', 'Cu', 'φ', 'σu', 'σspec, Sm/cm',
                 'logσspec', 'ε\'', 'ε\"', 'β\'', 'β\"', 'tanδ', 'M\'', 'M\"'], sep=';', index=False)
 
-
-current_dir = os.path.basename(os.getcwd())  # Get name of current directory
-out_dir = 'Zview_files'  # Directory for Zview out files
-data_dir = 'Data_txt'  # Directory for txt files
-# Check if you have already run the program and got the files.
-if os.path.exists(out_dir) and os.path.exists(data_dir) and glob.glob('*.xlsx'):
-    print("You have already generated necessary files.")
-else:
-    get_user_input()
+def makedir(name):
     try:
-        os.mkdir(out_dir)
-        os.mkdir(data_dir)
+        os.mkdir(name)
     except FileExistsError:
-        print('Warning!!! Files will be rewritten...')
-    finally:
-        # Generate or rewrite all files in any cases
-        with pd.ExcelWriter(f'{current_dir}_h={H}_d={D}.xlsx') as writer:
-            for i in sorted(glob.glob('*.txt')):
-                data_reading()
-                data_processing()
-                export_data_excel()
-                export_data_zview()
-                export_data_as_txt()
-        print("Processing of your absorption data is finished successfully!")
+        print('Warning!!! Files have already been generated')
+
+
+# Get name of current directory
+current_dir = os.path.basename(os.getcwd())
+# Check if you have already run the program and got all generated files.
+generated_dirs = {'Zview_files': os.path.exists('Zview_files'),
+                   'Data_txt': os.path.exists('Data_txt'),
+                  }
+
+if all([file==True for file in generated_dirs.values()]) \
+        and glob.glob(f'{current_dir}*.xlsx'):
+    print("You have already generated necessary files.")
+
+for dir, item in generated_dirs.items():
+    if item == False:
+        makedir(dir)
+
+user_params = get_user_input()
+
+with pd.ExcelWriter(f'{current_dir}_h={H}_d={D}.xlsx') as writer:
+    for i in sorted(glob.glob('*.txt')):
+        current_data_frame = data_reading()
+        data_processing(current_data_frame, *user_params)
+        export_data_excel(current_data_frame)
+        export_data_zview(current_data_frame, dir_name='Zview_files')
+        export_data_as_txt(current_data_frame, dir_name='Data_txt')
+print("Processing of your absorption data is finished successfully!")
