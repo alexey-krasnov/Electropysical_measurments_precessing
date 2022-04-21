a0447be6f0918630f41cc77fc3011a825f743cb1
Aleksei Krasnov <alexey-krasnov@rambler.ru>
2022-04-21 08:50:03 +0300

    Make test for get_user_input function

--- test_electric_processing.py
+++ test_electric_processing.py
@@ -1,3 +1,17 @@
 import unittest
 import pytest
+import electric_processing
 
+
+class ElectricTastCase(unittest.TestCase):
+    """Tests for electric_processing.py """
+
+    def test_get_user_input(self):
+        """Check if user input is not zero"""
+        h, d = electric_processing.get_user_input()
+        self.assertTrue(h)
+        self.assertTrue(d)
+
+
+if __name__ == '__main__':
+    unittest.main()
